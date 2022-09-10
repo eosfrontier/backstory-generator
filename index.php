eff9e39d1502ead919332203666ce9f637155c6b
Rolf <62988563+rolf-yoast@users.noreply.github.com>
2022-09-10 11:21:57 +0200

    basic setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="/assets/css/style.css" />
+<script src="/vendor/tinymce/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+      tinymce.init({
+        selector: '#mytextarea'
+      });
+    </script>
+</head>
+<body>
+<header>
+	<h1>Character name - backstory</h1>
+	Backstory status <span>In review</span>
+</header>
+<main>
+	<form method="post">
+		<textarea id="mytextarea">Hello, World!</textarea>
+    </form>
+</main>
+<footer>
+	  Hier komt de footer
+</footer>
+</body>
+</html>
