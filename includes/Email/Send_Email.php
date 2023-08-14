f888e9673a2111cd2ff817dfb2b70c66adfa1c07
mrperson123 <mrperson123@users.noreply.github.com>
2023-08-14 17:29:19 +0200

    Use env for email settings

--- includes/Email/Send_Email.php
+++ includes/Email/Send_Email.php
@@ -2,25 +2,26 @@
 namespace Eos\Backstory_generator\Email;
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 
 class Send_Email {
 
 	public function send_concept_changes_email() {
+
 		$mail = new PHPMailer();
 
 		$mail->isSMTP();
-		$mail->Host     = 'email-smtp.eu-west-1.amazonaws.com';
-		$mail->SMTPAuth = true;
-		$mail->Username = '';
-		$mail->Password = '';
-		$mail->Port     = 25;
+		$mail->Host       = $_ENV['email_host'];
+		$mail->SMTPAuth   = true;
+		$mail->Username   = $_ENV['email_user'];
+		$mail->Password   = $_ENV['email_pass'];
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->Port       = 587;
 
 		$mail->setFrom( 'spelleider@eosfrontier.space', 'Frontier SL-team' );
 		$mail->Subject = 'Concept changes requested';
 		$mail->Body    = 'test email';
 
-		$mail->addAddress( 'rolfsiebers@gmail.com' );
+		$mail->addAddress();
 
 		$mail->send();
 	}
