c5c48f031a875b46b642ed205334f88a434ed71e
mrperson123 <mrperson123@users.noreply.github.com>
2023-09-30 12:23:11 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@ $dotenv->load();
 
 require_once '../includes/SSO.php';
 
-if ( in_array( [ '30', '32' ], $jgroups, true ) ) {
+if ( ! in_array( [ '30', '32' ], $jgroups, true ) ) {
 	die( 'Sorry, you don\'t have access here. Naughty person' );
 }
 
