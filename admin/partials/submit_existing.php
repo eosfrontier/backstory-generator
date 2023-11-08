0995c533cffcb71ee871d1c05c84f7698b711a8a
WafflesMcDuff <goldjd@gmail.com>
2023-11-08 10:55:01 +0100

    Submit existing dropdown now respects faction filter

--- admin/partials/submit_existing.php
+++ admin/partials/submit_existing.php
@@ -1,15 +1,17 @@
-
 <?php $characters = $api->get_all_active_characters_no_backstory(); ?>
 <form method="POST">
 	<select name="character" onchange="this.form.submit()">
 		<option value="">Choose a character</option>
 		<?php
 		foreach ($characters as $char) {
-			if (isset($_POST['character']))	{
-				$selected = $_POST['character'] == $char['characterID'] ? 'selected' : '';
-				echo '<option value="' . $char['characterID'] . '" '. $selected .'  >' . $char['character_name'] . '</option>';
+			if (isset($_GET['faction']) && $_GET['faction'] != '' && $char['faction'] != $_GET['faction']) {
+				continue; #skips the character if the faction filter is chosen and the character is not a member of that faction
 			}
-			else echo '<option value="' . $char['characterID'] . '"  >' . $char['character_name'] . '</option>';
+			if (isset($_POST['character'])) {
+				$selected = $_POST['character'] == $char['characterID'] ? 'selected' : '';
+				echo '<option value="' . $char['characterID'] . '" ' . $selected . '  >' . $char['character_name'] . '</option>';
+			} else
+				echo '<option value="' . $char['characterID'] . '"  >' . $char['character_name'] . '</option>';
 		}
 		?>
 
@@ -25,20 +27,20 @@ if (isset($_POST['character'])) {
 			<?php echo $character->get_character_name($_POST['character']); ?>
 		</h2>
 		<div id="backstory-editor" style="display: block;">
-		<form method="post">
-		<textarea name="backstory-content" id="existing-backstory-textarea">
-			<?php
-			if ( isset($backstory->content) ){ 
-			echo $backstory->content;
-			} 
-			?>
-		</textarea>
-		<input type="hidden" name="type" value="backstory" />
-		<input type="hidden" name="status" value="approved" />
-		<input type="hidden" name="method" value="sl_backend" />
-		<input type="hidden" name="id" value="<?php echo $_POST['character']; ?>" />
-		<button class="submit-backstory button button--primary">Submit Backstory</button>
-	</form>
+			<form method="post">
+				<textarea name="backstory-content" id="existing-backstory-textarea">
+				<?php
+				if (isset($backstory->content)) {
+					echo $backstory->content;
+				}
+				?>
+			</textarea>
+				<input type="hidden" name="type" value="backstory" />
+				<input type="hidden" name="status" value="approved" />
+				<input type="hidden" name="method" value="sl_backend" />
+				<input type="hidden" name="id" value="<?php echo $_POST['character']; ?>" />
+				<button class="submit-backstory button button--primary">Submit Backstory</button>
+			</form>
 		</div>
 	</div>
 	<?php
